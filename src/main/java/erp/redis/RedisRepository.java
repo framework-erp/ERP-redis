11c215f964545562c1ebfb7d8c1c404bccc1e22d
zcd <zcd@wooduan2019>
2023-02-08 11:59:29 +0800

    开放底层资源给子类

--- src/main/java/erp/redis/RedisRepository.java
+++ src/main/java/erp/redis/RedisRepository.java
@@ -5,6 +5,9 @@ import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 
 public class RedisRepository<E, ID> extends Repository<E, ID> {
+    protected RedisTemplate<Object, Object> redisTemplate;
+    protected RedissonClient redissonClient;
+
     protected RedisRepository(RedisTemplate<Object, Object> redisTemplate, RedissonClient redissonClient) {
         this(redisTemplate, redissonClient, 30000);
     }
@@ -13,6 +16,8 @@ public class RedisRepository<E, ID> extends Repository<E, ID> {
         super(new RedisStore<>(redisTemplate), new RedisMutexes<>(redissonClient, maxLockTime));
         ((RedisStore) store).setEntityType(entityType);
         ((RedisMutexes) mutexes).setEntityType(entityType);
+        this.redisTemplate = redisTemplate;
+        this.redissonClient = redissonClient;
     }
 
     public RedisRepository(RedisTemplate<Object, Object> redisTemplate, RedissonClient redissonClient, Class<E> entityType) {
@@ -21,5 +26,7 @@ public class RedisRepository<E, ID> extends Repository<E, ID> {
 
     public RedisRepository(RedisTemplate<Object, Object> redisTemplate, RedissonClient redissonClient, long maxLockTime, Class<E> entityType) {
         super(new RedisStore<>(redisTemplate), new RedisMutexes<>(redissonClient, maxLockTime), entityType);
+        this.redisTemplate = redisTemplate;
+        this.redissonClient = redissonClient;
     }
 }
