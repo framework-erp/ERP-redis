24f2b2bbbf884043ac2062499e590e8c78781f4d
neo <13758242402@163.com>
2023-02-15 19:49:29 +0800

    优化repo构造器

--- src/main/java/erp/redis/RedisMutexes.java
+++ src/main/java/erp/redis/RedisMutexes.java
@@ -4,7 +4,10 @@ import erp.repository.Mutexes;
 import org.redisson.api.RBucket;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
+import java.lang.reflect.Field;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -17,18 +20,37 @@ public class RedisMutexes<ID> implements Mutexes<ID> {
     private boolean mock;
     private long maxLockTime;
 
-    public RedisMutexes(RedissonClient redissonClient, long maxLockTime) {
-        this(redissonClient, maxLockTime, null);
+    public RedisMutexes(RedisTemplate<String, Object> redisTemplate, long maxLockTime) {
+        this(redisTemplate, maxLockTime, null);
     }
 
-    public RedisMutexes(RedissonClient redissonClient, long maxLockTime, String entityType) {
-        if (redissonClient == null) {
+    public RedisMutexes(RedisTemplate<String, Object> redisTemplate, long maxLockTime, String entityType) {
+        if (redisTemplate == null) {
             mock = true;
             return;
         }
-        this.redissonClient = redissonClient;
+        this.redissonClient = getRedissonClientFromTemplate(redisTemplate);
         this.maxLockTime = maxLockTime;
         this.entityType = entityType;
+    }
+
+    private RedissonClient getRedissonClientFromTemplate(RedisTemplate<String, Object> redisTemplate) {
+        if (redisTemplate == null) {
+            return null;
+        }
+        RedissonConnectionFactory redissonConnectionFactory = (RedissonConnectionFactory) redisTemplate.getConnectionFactory();
+        Field fieldRedisson;
+        try {
+            fieldRedisson = RedissonConnectionFactory.class.getDeclaredField("redisson");
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException("field 'redisson' not found in RedissonConnectionFactory", e);
+        }
+        fieldRedisson.setAccessible(true);
+        try {
+            return (RedissonClient) fieldRedisson.get(redissonConnectionFactory);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("can not get field 'redisson' from RedissonConnectionFactory", e);
+        }
     }
 
     @Override
--- src/main/java/erp/redis/RedisRepository.java
+++ src/main/java/erp/redis/RedisRepository.java
@@ -1,13 +1,10 @@
 package erp.redis;
 
 import erp.repository.Repository;
-import org.redisson.api.RedissonClient;
-import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,42 +17,21 @@ public class RedisRepository<E, ID> extends Repository<E, ID> {
     }
 
     protected RedisRepository(RedisTemplate<String, Object> redisTemplate, long maxLockTime) {
-        super(new RedisStore<>(redisTemplate), new RedisMutexes<>(null, maxLockTime));
+        super(new RedisStore<>(redisTemplate), new RedisMutexes<>(redisTemplate, maxLockTime));
         ((RedisStore) store).setEntityType(entityType);
         ((RedisMutexes) mutexes).setEntityType(entityType);
-        ((RedisMutexes) mutexes).setRedissonClient(getRedissonClientFromTemplate(redisTemplate));
         this.redisTemplate = redisTemplate;
     }
 
-    public RedisRepository(RedisTemplate<String, Object> redisTemplate, RedissonClient redissonClient, Class<E> entityType) {
-        this(redisTemplate, redissonClient, 30000, entityType);
+    public RedisRepository(RedisTemplate<String, Object> redisTemplate, Class<E> entityType) {
+        this(redisTemplate, 30000, entityType);
     }
 
-    public RedisRepository(RedisTemplate<String, Object> redisTemplate, RedissonClient redissonClient, long maxLockTime, Class<E> entityClass) {
-        super(new RedisStore<>(redisTemplate), new RedisMutexes<>(redissonClient, maxLockTime), entityClass.getName());
+    public RedisRepository(RedisTemplate<String, Object> redisTemplate, long maxLockTime, Class<E> entityClass) {
+        super(new RedisStore<>(redisTemplate), new RedisMutexes<>(redisTemplate, maxLockTime), entityClass.getName());
         ((RedisStore) store).setEntityType(entityType);
         ((RedisMutexes) mutexes).setEntityType(entityType);
-        ((RedisMutexes) mutexes).setRedissonClient(getRedissonClientFromTemplate(redisTemplate));
         this.redisTemplate = redisTemplate;
-    }
-
-    private RedissonClient getRedissonClientFromTemplate(RedisTemplate<String, Object> redisTemplate) {
-        if (redisTemplate == null) {
-            return null;
-        }
-        RedissonConnectionFactory redissonConnectionFactory = (RedissonConnectionFactory) redisTemplate.getConnectionFactory();
-        Field fieldRedisson;
-        try {
-            fieldRedisson = RedissonConnectionFactory.class.getDeclaredField("redisson");
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException("field 'redisson' not found in RedissonConnectionFactory", e);
-        }
-        fieldRedisson.setAccessible(true);
-        try {
-            return (RedissonClient) fieldRedisson.get(redissonConnectionFactory);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("can not get field 'redisson' from RedissonConnectionFactory", e);
-        }
     }
 
     public List<String> queryAllIds() {
